fa5af5a3038d3073bd8d9fd30b8135ff91087917
Ashutosh Tripathi <ashu.t.500500@gmail.com>
2026-01-03 20:55:20 +0530

    updates

---
+++ backend/src/models/User.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    profileImage: {
+        type: String,
+        default: ""
+    },
+    clerkId: {
+        type: String,
+        required: true,
+        unique: true
+    }
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import path from "path";
+import cors from "cors";
+import { serve } from 'inngest/express';
+import { inngest } from './lib/inngest.js';
 import { ENV } from './lib/env.js';
 import { connectionDB } from './lib/db.js';
 
 const app = express();
 
 const __dirname = path.resolve();
+
+//middlewares
+app.use(express.json())
+app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));
+app.use("/api/inngest",serve({client: inngest, functions}))
 
 app.get('/health', (req, res) => {
     res.status(200).json({ msg: "Hello from server side!" })
