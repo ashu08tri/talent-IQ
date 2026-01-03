d4b16206d5568172ea502c39317755a0ac36c808
Ashutosh Tripathi <ashu.t.500500@gmail.com>
2026-01-03 21:17:21 +0530

    updates

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-import { connectDB } from "./db.js";
+import { connectionDB } from "./db.js";
 import User from "../models/User.js";
 // import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
@@ -9,7 +9,7 @@ const syncUser = inngest.createFunction(
   { id: "sync-user" },
   { event: "clerk/user.created" },
   async ({ event }) => {
-    await connectDB();
+    await connectionDB();
 
     const { id, email_addresses, first_name, last_name, image_url } = event.data;
 
@@ -34,7 +34,7 @@ const deleteUserFromDB = inngest.createFunction(
   { id: "delete-user-from-db" },
   { event: "clerk/user.deleted" },
   async ({ event }) => {
-    await connectDB();
+    await connectionDB();
 
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
